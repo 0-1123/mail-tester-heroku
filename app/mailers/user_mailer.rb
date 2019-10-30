02932d6886c12f8852ab4d8491f5a757b4d02d08
Alisia Michel <cc0-11@outlook.de>
2019-10-30 22:49:26 +0100

    Test Mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'connectus.enactus@gmail.com'
 
   def test_email
-    mail(to: "alisia.michel@whu.edu", subject: "Hello World")
+    mail(to: "jerome.soelter@whu.edu", subject: "Test-Email ConnectUs")
+    mail(to: "olivia.nick@whu.edu", subject: "Test-Email ConnectUs")
+    mail(to: "alisia.michel@whu.edu", subject: "Test-Email ConnectUs")
   end
 end
