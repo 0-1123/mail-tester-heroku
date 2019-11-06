030e15155a6702e6aab684020f14a4fcbb305f7c
Alisia Michel <cc0-11@outlook.de>
2019-11-06 17:07:47 +0100

    Email at Jerome

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,5 @@ class UserMailer < ApplicationMailer
 
   def test_email
     mail(to: "jerome.soelter@whu.edu", subject: "Test-Email ConnectUs")
-    mail(to: "olivia.nick@whu.edu", subject: "Test-Email ConnectUs")
-    mail(to: "alisia.michel@whu.edu", subject: "Test-Email ConnectUs")
   end
 end
