cf77b2fab09a33f0663252359e5d6cf18c1f8bb1
Alisia Michel <cc0-11@outlook.de>
2019-11-06 17:13:54 +0100

    Mail to many

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'connectus.enactus@gmail.com'
 
   def test_email
-    mail(to: "jerome.soelter@whu.edu", subject: "Test-Email ConnectUs")
-    mail(to: "olivia.nick@whu.edu", subject: "Test-Email ConnectUs")
-    mail(to: "alisia.michel@whu.edu", subject: "Test-Email ConnectUs")
+    mail(to: "jerome.soelter@whu.edu, olivia.nick@whu.edu, alisia.michel@whu.edu", subject: "Test-Email ConnectUs")
   end
 end
